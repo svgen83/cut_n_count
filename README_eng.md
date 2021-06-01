725e6c7f6c5bbfcb531f0d3a6cad6d5ac9b1a7d2
svg83 <svgeneraloff@gmail.com>
2021-06-01 13:38:51 +0400

    Initial commit

---
+++ README_eng.md
@@ -0,0 +1,14 @@
+# cut_n_count
+ The program converts long Internet links into bitlinks, and also counts the number of clicks on bitlinks
+
+### How to install
+
+Python3 should be already installed. 
+Then use `pip` (or `pip3`, if there is a conflict with Python2) to install dependencies:
+```
+pip install -r requirements.txt
+```
+
+### Project Goals
+
+The code is written for educational purposes on online-course for web-developers [dvmn.org](https://dvmn.org/).
---
+++ cut_n_count.py
@@ -0,0 +1,64 @@
+import requests
+from urllib.parse import urlparse
+import os
+from dotenv import load_dotenv
+import argparse
+
+
+def shorten_link(token, url):
+    bitlink_endpoint = "https://api-ssl.bitly.com/v4/bitlinks"
+    bitlink_header = {"Authorization": token}
+    bitlink_body = {"long_url": url}
+    response = requests.post(bitlink_endpoint,
+                             json=bitlink_body,
+                             headers=bitlink_header)
+    response.raise_for_status()
+    bitlink_resp = response.json()
+    return bitlink_resp["link"]
+
+
+def count_clicks(token, link_without_scheme):
+    url_template = "https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary"
+    clicks_endpoint = url_template.format(link_without_scheme)
+    headers = {"Authorization": token}
+    params = {"unit": "day", "units": -1}
+    response = requests.get(clicks_endpoint, headers=headers, params=params)
+    response.raise_for_status()
+    clicks = response.json()
+    return clicks["total_clicks"]
+
+
+def delete_scheme_url(url):
+    url_fragments = urlparse(url)
+    return "".join(url_fragments[1:])
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    parser = argparse.ArgumentParser(description="""
+    Сокращение длинных ссылок и
+    подсчет перехода по коротким ссылкам
+    """)
+    parser.add_argument("url", help="input e-link")
+    args = parser.parse_args()
+    url = args.url
+    link_without_scheme = delete_scheme_url(url)
+    token = os.getenv("BITLY_TOKEN")
+    error_msg = """
+    Ошибка. Страница не найдена.
+    Возможно, Вы указали не ту ссылку
+    или ошиблись при вводе.
+    Введите ссылку заново
+    """
+    bitlink_endpoint_template = "https://api-ssl.bitly.com/v4/bitlinks/{}"
+    bitlink_endpoint = bitlink_endpoint_template.format(link_without_scheme)
+    headers = {"Authorization": token}
+    response = requests.get(bitlink_endpoint, headers=headers)
+    try:
+        if response.ok:
+            print("Количество переходов по короткой ссылке: ",
+                  count_clicks(token, link_without_scheme))
+        else:
+            print("Короткая ссылка: ", shorten_link(token, url))
+    except requests.HTTPError:
+        print(error_msg)
